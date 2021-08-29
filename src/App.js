ac7070e2d324f8182313f3b35330470d5eaf2b6c
Mahan Esmaeilzadeh <mesmaeilzadeh@my.bcit.ca>
2021-08-29 16:51:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDHj6bckLNKC9QM561wyPnWVoyC5M0EZVs",
+  authDomain: "factswallet-bbf90.firebaseapp.com",
+  projectId: "factswallet-bbf90",
+  storageBucket: "factswallet-bbf90.appspot.com",
+  messagingSenderId: "592394552215",
+  appId: "1:592394552215:web:529dd4e38d0ae51259bad2",
+  measurementId: "G-37PF0YTJ12"})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+  }
+
+
+  return (
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+  )
+}
+
 
 function App() {
+  
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <section>
+        {user ? <Facts /> : <SignIn />}
+      </section>
     </div>
   );
 }
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function SignIn() {
+
+    const signInWithGoogle = () => {
+        const provider = new fire
+    }
+
+
+    return (
+        <button onClick={signInWithGoogle}>Sign In With Google</button>
+    )
+}
